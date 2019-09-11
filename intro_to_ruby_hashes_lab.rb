e7e6834c829bd469bc4ff55016ed7f3669bb521f
Tenzin Paldon <tenzin.paldon16@gmail.com>
2019-09-11 05:06:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  {:k => key, :v => value}
+  {key => value}
   # return a hash that includes the key and value parameters passed into this method
 end
 
